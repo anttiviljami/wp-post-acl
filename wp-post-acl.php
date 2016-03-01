7560cc274762b0bfae7629b1e2fddaa79a7cb249
Antti Kuosmanen <antti@seravo.fi>
2016-03-01 22:41:14 +0200

    Enforce edit permissions successfully

--- wp-post-acl.php
+++ wp-post-acl.php
@@ -45,10 +45,62 @@ class WP_Post_ACL {
   private function __construct() {
     $this->post_types = defined('ACL_POST_TYPES') ? unserialize( ACL_POST_TYPES ) : [ 'post', 'page' ];
 
+    add_filter( 'user_has_cap', array( $this, 'check_post_edit_acl' ), 10, 3 );
+
     add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
     add_action( 'save_post', array( $this, 'save_permissions' ) );
 
     add_action( 'plugins_loaded', array( $this, 'load_our_textdomain' ) );
+  }
+
+  /**
+   * check_post_edit_acl()
+   *
+   * Filter on the current_user_can() function.
+   * Here we check if we need to remove the edit_post cap for a specific post
+   *
+   * @param array $allcaps All the capabilities of the user
+   * @param array $cap     [0] Required capability
+   * @param array $args    [0] Requested capability
+   *                       [1] User ID
+   *                       [2] Associated object ID
+   */
+  public function check_post_edit_acl( $allcaps, $cap, $args ) {
+    // we do this only for edit_post
+    if ( 'edit_post' != $args[0] )
+      return $allcaps;
+
+    // post id must be set
+    if ( !isset( $args[2] ) ) {
+      return $allcaps;
+    }
+
+    // post data
+    $post = get_post( $args[2] );
+
+    // are we concerned about this post type
+    if( !in_array( $post->post_type, $this->post_types ) ) {
+      return $allcaps;
+    }
+
+    // user id
+    $user_id = $args[1];
+
+    // are we concerned about this user?
+    $editors = self::get_editors();
+    $editor_ids = array_map( function( $user ) { return $user->ID; }, $editors );
+    if( !in_array( $user_id, $editor_ids ) ) {
+      return $allcaps;
+    }
+
+    // does user have permissions to edit this post
+    if( $this->has_edit_permissions( $post->ID, $user_id ) ) {
+      return $allcaps;
+    }
+
+    $allcaps[$cap[0]] = false;
+
+    return $allcaps;
   }
 
   public function add_meta_box() {
@@ -91,10 +143,10 @@ class WP_Post_ACL {
     // convert $user to WP_User if not yet an instance
     if( ! $user instanceof WP_User ) {
       if( is_numeric( $user ) ) {
-        $user = get_user_by( $user, 'slug' );
+        $user = get_user_by( 'id', $user );
       }
       else {
-        $user = get_user_by( $user, 'id' );
+        $user = get_user_by( 'slug', $user );
       }
     }
 
